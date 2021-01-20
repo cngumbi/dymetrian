8193e70b1da9f44900f8b1124283bbeae6fb5930
cngumbi <cngumbi35@gmail.com>
2021-01-20 14:48:45 +0300

    new changes

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Configuration(object):
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))                   #applicatioon directory
+    DEBUG = True                                                                    #the debug is set to true
+    SECRET_KEY = '&\xfb?\xfbL\xd7\xc0z\x19ewF\xdd\xe6\xce(M\xbc\x15,'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/database/dymetrian.db' %APPLICATION_DIR          #setting the location to save our database
+    STATIC_DIR = os.path.join(APPLICATION_DIR, 'static')                            #identifying the location of our static file
+    IMAGES_DIR = os.path.join(STATIC_DIR, 'images')                                 #setting the location to save our image files
